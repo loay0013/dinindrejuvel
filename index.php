bcad42ba6ff6776ddd6482b6bae6294b12da4a67
MikkelLebedev <mikkel.lebedev@outlook.com>
2023-05-11 10:39:58 +0200

    Forside 1.0

--- index.php
+++ index.php
@@ -15,9 +15,39 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-Beige">
 <!--navbar-->
 <?php include "includes/nav.php"?>
+
+<div class="">
+	<div class="hero">
+		<div class="row m-0">
+			<div class="col-md-6 px-0">
+				<img src="img/pexels-cottonbro-studio-3737576.jpg" alt="">
+			</div>
+			<div class="col-md-6 px-0">
+				<img src="img/pexels-cup-of-couple-6963132.jpg" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div>
+	<div class="row d-flex justify-content-center  my-md-6">
+		<div class="container-about  d-flex flex-md-row flex-column justify-content-between col-12 align-items-center ">
+			<div class="col-md-6 flex-column col-12 d-flex flex-md-row">
+				<div class="d-flex flex-column">
+					<h2 class="">Holistisk Blog</h2>
+					<br><p>Vores holistiske blog handler typisk om tilgangen til livet, der man ser på mennesket som en helhed, der er forbundet med alt omkring sig.
+						En holistisk tilgang kan anvendes inden for mange forskellige områder, fx sundhed, miljø, relationer og spiritualitet.
+						Vi kommer ind på spirituelle og holistiske ritualer, begreber og produkter. </p>
+				</div>
+			</div>
+				<div class="col-md-6  col-12 flex-column flex-md-row img-bg align-items-center d-flex me-md-5 mt-5 mt-md-0">
+				</div>
+			</div>
+		</div>
+	</div>
 
 
 
