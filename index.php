78b154a3535391ab15bbf0fab60beb816f0d902d
MikkelLebedev <mikkel.lebedev@outlook.com>
2023-05-12 12:18:44 +0200

    Forside 1.4 + ny sektion samt nye billeder i svg format

--- index.php
+++ index.php
@@ -56,9 +56,10 @@
 
 
 
-<div class="">
+<div class="d-flex flex-md-row flex-column justify-content-md-around container mt-m">
+	<div class="d-flex flex-column">
 	<div>
-		<h3>Seneste Post</h3>
+		<h3 class="mb-4">Seneste Post</h3>
 	</div>
 	<div class="">
         <?php
@@ -67,12 +68,12 @@
         ?>
 		<div class="">
 			<?php foreach ($blog as $blog) { ?>
-			<div class="col-md-4 col-12">
+			<div class="col-md-10 col-12 gap-4">
 				<a  href="blog.php?BlogId=${items.BlogId}" class="text-decoration-none text-dark d-md-flex ">
 					<div class="">
-						<img src="uploads/<?php echo $blog->blogBillede;?>" alt="<?php echo $blog->blogSeoAlt;?>">
+						<img class="wh-img1" src="uploads/<?php echo $blog->blogBillede;?>" alt="<?php echo $blog->blogSeoAlt;?>">
 						<div class="card-body">
-							<h4 class="card-title"><?php echo $blog->blogOverskrift ?></h4>
+							<h4 class="card-title mt-3 mb-5"><?php echo $blog->blogOverskrift ?></h4>
 						</div>
 					</div>
 				</a>
@@ -80,19 +81,22 @@
 			<?php } ?>
 		</div>
 	</div>
-    <div>
+</div>
+
+
+    <div class="d-flex flex-md-row flex-column col-md-6 col-12 mt-md-5">
     <?php
        $blog = $db->sql("SELECT * FROM blog ORDER BY blogId DESC LIMIT 4 OFFSET 1");
     ?>
-        <div>
+        <div class="d-flex flex-md-wrap flex-md-row flex-column col-12 mt-md-4 pt-2">
             <?php foreach ($blog as $blog) { ?>
-                <div class="col-md-4 col-12">
+                <div class="col-md-5 col-12">
                     <div>
                     <a  href="blog.php?BlogId=${items.BlogId}" class="text-decoration-none text-dark d-md-flex ">
                         <div class="">
-                            <img src="uploads/<?php echo $blog->blogBillede;?>" alt="<?php echo $blog->blogSeoAlt;?>">
+                            <img class="wh-img" src="uploads/<?php echo $blog->blogBillede;?>" alt="<?php echo $blog->blogSeoAlt;?>">
                             <div class="card-body">
-                                <h4 class="card-title"><?php echo $blog->blogOverskrift ?></h4>
+                                <h4 class="card-title mt-3 mb-5"><?php echo $blog->blogOverskrift ?></h4>
                             </div>
                         </div>
                     </a>
@@ -105,6 +109,36 @@
 
 
 
+<div>
+	<div class="d-flex justify-content-md-start justify-content-center p-d1">
+		<img class="img-flower1" src="img/flower%201.svg" alt="">
+	</div>
+<div class="bg-Blå1 d-flex flex-md-row flex-column">
+	<div class="container col-12 col-md-4 bg-Grøn1 d-flex flex-column flex-md-row my-5 z-index  " >
+		<img class="wh-img" src="img/Mission-img.png">
+		<div class="d-flex flex-column">
+			<h4> Din Indre Juvel webshop</h4>
+			<p> Din Indre Juvel webshop</p>
+			<button></button>
+		</div>
+	</div>
+	<div class="container col-12 col-md-4 bg-Blå2 my-5 d-flex flex-md-row flex-column " >
+		<div class="d-flex flex-column flex-md-row">
+			<img class="wh-img" src="img/Mission-img.png">
+			<div class="d-flex flex-row">
+				<img src="img/Icon%20simple-tiktok.svg">
+				<img src="img/Icon%20simple-tiktok.svg">
+				<img src="img/Icon%20simple-tiktok.svg">
+			</div>
+			<div class="d-flex flex-column">
+				<h3>Følg os</h3>
+				<p>Instagram</p>
+				<button></button>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
 
 
 
